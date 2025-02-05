e1a5f86f86d8edd4b2d3c9e06f36c0ffb1aa42fe
[aslopeys941@gamail.com] <[aslopeys941@gmail.com]>
2025-02-06 01:30:16 +0900

    chore: 1차댓글 CRUD 기능을 PR하기전 커밋 (grpc폴더 파일 2개와, RsaKeyManager 파일 주석 풀었음)

--- src/main/java/org/noteam/be/comment/domain/Comment.java
+++ src/main/java/org/noteam/be/comment/domain/Comment.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
-
+ 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
