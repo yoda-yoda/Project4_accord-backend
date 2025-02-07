b9ab443534ed726199fe68ceba57132231c3c3cb
[aslopeys941@gamail.com] <[aslopeys941@gmail.com]>
2025-02-07 14:59:23 +0900

    fix: 테스트 코드 빨간줄 수정 등 PR전 점검중

--- src/main/java/org/noteam/be/comment/controller/CommentController.java
+++ src/main/java/org/noteam/be/comment/controller/CommentController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/comment")
+@RequestMapping("/api/members/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
--- src/main/java/org/noteam/be/joinBoard/controller/JoinBoardController.java
+++ src/main/java/org/noteam/be/joinBoard/controller/JoinBoardController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/join-board")
+@RequestMapping("/api/members/join-board")
 @RequiredArgsConstructor
 public class JoinBoardController {
 
